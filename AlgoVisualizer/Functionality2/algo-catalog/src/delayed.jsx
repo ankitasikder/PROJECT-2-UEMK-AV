0556dc3c89cbb159684af19663e7416976cd081f
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:25:20 +0530

    Update delayed.jsx

--- AlgoVisualizer/Functionality2/algo-catalog/src/delayed.jsx
+++ AlgoVisualizer/Functionality2/algo-catalog/src/delayed.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+/*ALL THESE WORKS ARE DONE BY 
 
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 class Delayed extends React.Component {
 
     constructor(props) {
