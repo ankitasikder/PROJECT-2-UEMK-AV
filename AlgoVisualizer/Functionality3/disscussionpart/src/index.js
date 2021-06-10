47d9f875bc52ca219101a28a378c73307996beec
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:35:59 +0530

    Update index.js

--- AlgoVisualizer/Functionality3/disscussionpart/src/index.js
+++ AlgoVisualizer/Functionality3/disscussionpart/src/index.js
@@ -4,3 +4,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 setTimeout(()=>ReactDOM.render(<App />, document.getElementById('root')),3000);
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
