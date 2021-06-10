ea140ed4b01ab0d52fd9e30592bf9505f8bbb960
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:50:48 +0530

    by ankita

--- algo-visualize-loginSignup/src/components/Login/formikConfig.js
+++ algo-visualize-loginSignup/src/components/Login/formikConfig.js
@@ -1,5 +1,14 @@
 import * as Yup from 'yup';
+/*ALL THESE WORKS ARE DONE BY 
 
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 export const defaultValues = {
   email: '',
   password: '',
