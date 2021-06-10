7266609cee68da7c55f3dd6a90217ef8cfd86714
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:42:10 +0530

    by ankita

--- AlgoVisualizer/Functionality4/feedback/src/pages/About.js
+++ AlgoVisualizer/Functionality4/feedback/src/pages/About.js
@@ -1,5 +1,15 @@
 export const About = () => {
     return (
+        /*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
         <section>
       <div class="jumbotron text-center">
   <h1 className="sak">Algorithm Visualizer About Page</h1>
