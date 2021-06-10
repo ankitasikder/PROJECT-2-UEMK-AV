97b6fea77176b5a5b848cb8e1263721ec2d4a5a1
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:35:48 +0530

    by ankita

--- AlgoVisualizer/Functionality3/disscussionpart/src/App.js
+++ AlgoVisualizer/Functionality3/disscussionpart/src/App.js
@@ -1,5 +1,14 @@
 import { ChatEngine } from 'react-chat-engine';
+/*ALL THESE WORKS ARE DONE BY 
 
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
