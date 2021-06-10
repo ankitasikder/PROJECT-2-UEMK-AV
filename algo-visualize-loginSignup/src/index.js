475d9bb8961ed471c07433b734e4c93ba3bcda93
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:52:02 +0530

    by ankita

--- algo-visualize-loginSignup/src/index.js
+++ algo-visualize-loginSignup/src/index.js
@@ -3,7 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components';
 import { BrowserRouter as Router } from 'react-router-dom';
+/*ALL THESE WORKS ARE DONE BY 
 
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 setTimeout(() => {
   ReactDOM.render(
     <React.StrictMode>
