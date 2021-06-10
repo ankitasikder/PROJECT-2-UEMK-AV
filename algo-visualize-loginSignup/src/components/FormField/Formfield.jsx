0b5168a1656fe1122ad4aef8b75208aaf4d6ef0a
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:49:27 +0530

    by ankita

--- algo-visualize-loginSignup/src/components/FormField/Formfield.jsx
+++ algo-visualize-loginSignup/src/components/FormField/Formfield.jsx
@@ -1,5 +1,14 @@
 import { ErrorMessage, Field } from 'formik';
+/*ALL THESE WORKS ARE DONE BY 
 
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 export const FormField = ({ name, label, type = 'text' }) => (
   <label>
     {label}
