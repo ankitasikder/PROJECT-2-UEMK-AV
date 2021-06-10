cfa11f2433adaed7795f64c51950eb4059e551c5
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:43:31 +0530

    by ankita

--- AlgoVisualizer/Functionality4/feedback/src/pages/Userguide.js
+++ AlgoVisualizer/Functionality4/feedback/src/pages/Userguide.js
@@ -1,4 +1,14 @@
 export const Userguide = () => {
+    /*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
     return (
         <div>
             <div class="jumbotron">
